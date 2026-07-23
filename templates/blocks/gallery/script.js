90618aec3b7c86b9f831dc7543ea05d8904576ce
JulienB <brochard.ju@gmail.com>
2026-07-23 09:07:27 +0200

    gallery - passage à 4 image sur la ligne

--- templates/blocks/gallery/script.js
+++ templates/blocks/gallery/script.js
@@ -2,21 +2,23 @@
 	$('.cbo-gallery .gallery-list').slick({
 		arrows: false,
 		dots: true,
-		slidesToShow: 1,
+		slidesToShow: 2,
 		slidesToScroll: 1,
 		infinite: true,
 		centerMode: true,
-		centerPadding: '20%',
+		centerPadding: '10%',
 		responsive: [
 			{
 				breakpoint: 1024,
 				settings: {
+					slidesToShow: 1,
 					centerPadding: '120px',
 				}
 			},
 			{
 				breakpoint: 767,
 				settings: {
+					slidesToShow: 1,
 					centerPadding: '30px',
 				}
 			}
