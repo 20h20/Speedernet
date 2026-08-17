a1fc3464a88d28b18a598b9f65e967ce1ebe58ab
JulienB <brochard.ju@gmail.com>
2026-08-17 10:14:50 +0200

    ajout du code de tracking

--- header.php
+++ header.php
@@ -17,6 +17,16 @@
 		<?php
 			$hdbt = get_field('header_button', 'option');
 		?>
+
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-DL663BQ4HZ"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-DL663BQ4HZ');
+		</script>
 	</head>
 
 	<body <?php body_class('cbo-main'); ?> itemscope itemtype="https://schema.org/WebPage">
